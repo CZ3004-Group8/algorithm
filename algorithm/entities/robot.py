a064458d73f38e160bf94849b94a1a730007fb54
Daryl <aka.hjd@gmail.com>
2022-01-21 13:57:11 +0800

    feat: allow specifying turn direction (left or right)

--- algorithm/entities/robot.py
+++ algorithm/entities/robot.py
@@ -9,7 +9,7 @@ import pygame
 class Robot:
     ROBOT_LENGTH = 21 * SCALING_FACTOR  # Front to back
     ROBOT_WIDTH = 20 * SCALING_FACTOR  # Left to right
-    TURNING_RADIUS = 25 * SCALING_FACTOR  # Turning radius of the robot in centimeters.  Theoretical value is 25.
+    TURNING_RADIUS = 25 * SCALING_FACTOR  # Turning radius of the robot in centimeters. Theoretical value is 25.
 
     def __init__(self, x, y, angle):
         """
@@ -20,7 +20,7 @@ class Robot:
 
         self.color = colors.RED
 
-    def rotate(self, d_angle):
+    def rotate(self, d_angle, to_left):
         """
         x_new = x + R(sin(∆θ + θ) − sin θ)
         y_new = y − R(cos(∆θ + θ) − cos θ)
@@ -31,14 +31,22 @@ class Robot:
             - +ve ∆θ -> rotate counter-clockwise
             - -ve ∆θ -> rotate clockwise
         """
-        self.center.x += self.TURNING_RADIUS * (math.sin(self.angle + d_angle) - math.sin(self.angle))
-        self.center.y += self.TURNING_RADIUS * (math.cos(self.angle + d_angle) - math.cos(self.angle))
+        # Get change in (x, y) coordinate.
+        x_change = self.TURNING_RADIUS * (math.sin(self.angle + d_angle) - math.sin(self.angle))
+        y_change = self.TURNING_RADIUS * (math.cos(self.angle + d_angle) - math.cos(self.angle))
+
+        if to_left:  # Robot wants to turn left.
+            self.center.x += x_change
+            self.center.y += y_change
+        else:  # Robot wants to turn right.
+            self.center.x -= x_change
+            self.center.y += y_change
         self.angle += d_angle
 
     def update(self):
         # Rotate the image
         angle = 0.025
-        self.rotate(angle)  # Robot always rotating anti-clockwise by 0.1 radians.
+        self.rotate(angle, False)
 
     def draw(self, screen):
         self.update()
