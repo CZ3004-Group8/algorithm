5ccbd14ad99b333609cccc3763d6359a11436ff3
Daryl <aka.hjd@gmail.com>
2022-01-21 09:41:31 +0800

    chore: bring robot to the start box

--- algorithm/entities/grid.py
+++ algorithm/entities/grid.py
@@ -8,7 +8,10 @@ class Grid:
     WIDTH = 200  # The grid is 20x20 cells.
     CELL_WIDTH = 10  # Width in centimeters of one cell.
 
-    START_BOX_WIDTH = 40  # Width of the starting box for the robot.
+    START_BOX_WIDTH = 30  # Width of the starting box for the robot.
+
+    def __init__(self, obstacles):
+        self.obstacles = obstacles
 
     def draw_borders(self, screen):
         """
@@ -25,11 +28,17 @@ class Grid:
         pygame.draw.line(screen, colors.RED, (self.WIDTH * SCALING_FACTOR, 1),
                          (self.WIDTH * SCALING_FACTOR, self.WIDTH * SCALING_FACTOR))
 
+    def get_start_box_rect(self):
+        """
+        Get the Rect that shows the start box.
+        """
+        return pygame.Rect(0, self.WIDTH * SCALING_FACTOR - (self.START_BOX_WIDTH * SCALING_FACTOR),
+                           self.START_BOX_WIDTH * SCALING_FACTOR,
+                           self.START_BOX_WIDTH * SCALING_FACTOR)  # left, top, width, height
+
     def draw_start_box(self, screen):
         # Starting box
-        start_box = pygame.Rect(0, self.WIDTH * SCALING_FACTOR - (self.START_BOX_WIDTH * SCALING_FACTOR),
-                                self.START_BOX_WIDTH * SCALING_FACTOR,
-                                self.START_BOX_WIDTH * SCALING_FACTOR) # left, top, width, height
+        start_box = self.get_start_box_rect()
         pygame.draw.rect(screen, colors.GREEN, start_box)
 
     def update(self, screen):
--- algorithm/main.py
+++ algorithm/main.py
@@ -8,32 +8,48 @@ class AlgoApp:
     def __init__(self):
         pygame.init()
         self.running = False
-        self.size = self.width, self.height = 800, 800
+        self.size = self.width, self.height = 1000, 900
 
         self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.clock = pygame.time.Clock()
 
-        self.robot = Robot(400, 400, 0)
-        self.grid = Grid()
+        self.grid = Grid([])
+        # Get the starting coordinate of the robot.
+        start_pos = self.grid.get_start_box_rect().center
+        self.robot = Robot(*start_pos, 0)
 
     def settle_events(self):
+        """
+        Process Pygame events.
+        """
         for event in pygame.event.get():
+            # On quit, stop the game loop. This will stop the app.
             if event.type == pygame.QUIT:
                 self.running = False
 
     def init(self):
+        """
+        Set initial values for the app.
+        """
         self.running = True
         pygame.display.set_caption("Algorithm")
 
     def render(self):
+        """
+        Render the screen.
+        """
         self.screen.fill((255, 255, 255), None)
 
-        self.robot.update(self.screen)
         self.grid.update(self.screen)
+        self.robot.update(self.screen)
 
+        # Really render now.
         pygame.display.flip()
 
     def execute(self):
+        """
+        Initialise the app and start the game loop.
+        """
         self.init()
 
         while self.running:
