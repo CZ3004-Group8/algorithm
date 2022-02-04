08dd88c9d85dff883d43997bddbaad3e8ec8484e
Daryl <aka.hjd@gmail.com>
2022-02-04 09:15:42 +0800

    minor formatting

--- algorithm/entities/robot/brain.py
+++ algorithm/entities/robot/brain.py
@@ -65,7 +65,7 @@ class Brain:
         """
         # Get the current offset of the obstacle from the robot's perspective.
         offset_pos = self.wrt_bot(curr_pos, target_pos)
-        # TODO: Use the offset position to plan the path from robot's current position to target position.
+        # Find which quadrant the obstacle is wrt to the robot.
 
     @classmethod
     def wrt_bot(cls, bot_pos, target_pos) -> Position:
@@ -76,46 +76,24 @@ class Brain:
         # Get the x, y difference between the current and target
         x_diff, y_diff = target_pos.x - bot_pos.x, bot_pos.y - target_pos.y
 
-        # Figure out which quadrant the next target is
-        # WITH RESPECT TO the current location and orientation.
-        # Note that we use 0-indexing.
-        quad = 3
         facing = 0
-        if x_diff >= 0 and y_diff >= 0:
-            quad = 0
-            print("Next point in 1st quadrant.")
-        elif x_diff < 0 and y_diff >= 0:
-            quad = 1
-            print("Next point in 2nd quadrant.")
-        elif x_diff < 0 and y_diff < 0:
-            quad = 2
-            print("Next point in 3rd quadrant.")
-        else:
-            print("Next point in 4th quadrant.")
-
         # We change it to depend on the current orientation.
         if bot_pos.angle == 0:
             print("Robot currently facing east.")
-            quad += 1
             facing = 3
         elif bot_pos.angle == -math.pi / 2:
             print("Robot currently facing south.")
-            quad += 2
             facing = 2
         elif bot_pos.angle == math.pi:
             print("Robot currently facing west.")
-            quad += 3
             facing = 1
         else:
             print("Robot currently facing north.")
-        # Modulo and make it 1-indexed.
-        quad = (quad % 4) + 1
-        print(f"With respect to current position and orientation, obstacle is at quadrant {quad}")
 
-        # TODO: Junyan, do it here.
         # check target's next coordinates with respect to robot's pov
-        newcoord_x = bot_pos.x + (math.cos(facing*0.5) * (x_diff)) - (math.sin(facing*0.5) * (y_diff)) - bot_pos.x
-        newcoord_y = bot_pos.y + (math.sin(facing*0.5) * (x_diff)) + (math.cos(facing*0.5) * (y_diff)) - bot_pos.y
-        print(f"Target's new coordinate wrt to robot's POV is {newcoord_x/settings.SCALING_FACTOR}, {newcoord_y/settings.SCALING_FACTOR}")
+        newcoord_x = bot_pos.x + (math.cos(facing * 0.5) * x_diff) - (math.sin(facing * 0.5) * y_diff) - bot_pos.x
+        newcoord_y = bot_pos.y + (math.sin(facing * 0.5) * x_diff) + (math.cos(facing * 0.5) * y_diff) - bot_pos.y
+        print(f"Target's new coordinate wrt to robot's POV is {newcoord_x / settings.SCALING_FACTOR}, "
+              f"{newcoord_y / settings.SCALING_FACTOR}")
 
-        return Position(newcoord_x, newcoord_y,0)
+        return Position(newcoord_x, newcoord_y, 0)
