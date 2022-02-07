a13a2554380e0300a1ccdc562a86a7dd4fec4dbb
Daryl <aka.hjd@gmail.com>
2022-02-07 11:17:14 +0800

    just to save changes

--- algorithm/main.py
+++ algorithm/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # (x-coordinate, y-coordinate, Direction)
     obs = [
         Obstacle(30, 70, Direction.BOTTOM),
-        Obstacle(30, 130, Direction.BOTTOM),
+        Obstacle(30, 170, Direction.BOTTOM),
         Obstacle(100, 150, Direction.BOTTOM),
         Obstacle(150, 60, Direction.BOTTOM)
     ]
